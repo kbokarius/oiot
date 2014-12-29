c0f79ee97e7c81f4cbcf38f662f3486454d92661
Kon <bokarius@comcast.net>
2014-12-28 19:43:06 -0800

    added docstrings

--- run_curator.py
+++ run_curator.py
@@ -1,7 +1,18 @@
+"""
+    run_curator
+    ~~~~~~~~~
+    A convenience script for running the Curator as a service.
+    :copyright: (c) 2014 by Konstantin Bokarius.
+    :license: MIT, see LICENSE for more details.
+"""
 from oiot import Curator, OiotClient
 import sys, time, traceback
 
 if __name__ == '__main__':
+    """
+    Instantiate a Curator class instance and run it. If an exception
+    is caught then log it, sleep a few seconds, and continue running.
+    """
     api_key = sys.argv[1]
     if api_key is None:
         print('Error: specify the API key to use.')
